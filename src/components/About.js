272eb2f534c55689f94b23fe728118eaeb0a1bc1
caseytansey <ctansey12600@outlook.com>
2021-10-10 22:27:06 -0500

    commit tree a7a10edf281d8d2f0dfa5535b1b523393b948309

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,11 @@ import React from "react";
 import Links from "./Links"
 
 function About({ bio, links }) {
+  console.log(bio)
   return (
     <div id="about">
       <h2>About Me</h2>
-      {bio.length > 0 && 
+      {bio && bio.length > 0 &&
         <p>{bio}</p> }
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       <Links github={links.github} linkedin={links.linkedin}/>
