21f5e8964a4d8f9a7e6aa41aab0727a58689ce42
caseytansey <ctansey12600@outlook.com>
2021-10-10 22:09:11 -0500

    Completed Assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-function Home(props) {
+function Home({ username, city, color}) {
+  const fontColor = {color}
   return (
     <div id="home">
-      <h1 style={{ color: props.color }}>
-        {props.username} is a Web Developer from {props.city}
+      <h1 style={fontColor}>
+        {username} is a Web Developer from {city}
       </h1>
     </div>
   );
